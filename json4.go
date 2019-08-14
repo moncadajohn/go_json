511d9c7fe34d33a95e23c8f353a7d8d8800a871c
Moncada <jhonjamv@gmail.com>
2019-08-13 22:59:46 -0500

    Funciona en un 90%

--- json4.go
+++ json4.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -11,20 +10,23 @@ type Datos struct {
 }
 
 type Info struct {
-	Servers_changed    string
+	Servers_changed    bool
 	Ssl_grade          string
 	Previous_ssl_grade string
 	Logo               string
 	Title              string
-	Is_down            string
+	Is_down            bool
 }
 
 type Employee struct {
-	Servers []Datos
+	Servers  []Datos
+	Servers2 []Info
 }
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
 		data := Employee{
 
 			Servers: []Datos{
@@ -47,13 +49,32 @@ func main() {
 					Owner:     "Amazon.com, Inc.",
 				},
 			},
+			//}
+			//json.NewEncoder(w).Encode(data)
+
+			//file, _ := json.MarshalIndent(data, "", " ")
+			///aqui inicia el otro
+			//data = Employee{
+
+			Servers2: []Info{
+				Info{
+					Servers_changed:    true,
+					Ssl_grade:          "B",
+					Previous_ssl_grade: "A+",
+					Logo:               "https://server.com/icon.png",
+					Title:              "Title of the page",
+					Is_down:            false,
+				},
+			},
 		}
-
-		file, _ := json.MarshalIndent(data, "", " ")
-
-		_ = ioutil.WriteFile("test.json", file, 0644)
 		json.NewEncoder(w).Encode(data)
-
 	})
+
+	//	file, _ := json.MarshalIndent(data2, "", " ")
+
+	//	_ = ioutil.WriteFile("test.json", file, 0644)
+	//json.NewEncoder(w).Encode(data)
+	//json.NewEncoder(w).Encode(data2)
 	http.ListenAndServe(":8000", nil)
+
 }
